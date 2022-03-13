6211935466019aa25eb280ebe16b7d4039c8b7b3
SMJin <sstaw@naver.com>
2022-03-13 17:07:28 +0900

    first commit

---
+++ src/main/java/com/example/dalihouse/DaliHouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.dalihouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaliHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaliHouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dalihouse/DaliHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dalihouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaliHouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
