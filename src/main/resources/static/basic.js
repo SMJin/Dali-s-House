d23e436c9b1cbb23dffaf5c211e389db9093fdfb
SMJin <sstaw@naver.com>
2022-03-14 00:35:56 +0900

    디자인없는 html UI +update 기능추가 (임시)

--- src/main/resources/static/basic.js
+++ src/main/resources/static/basic.js
@@ -45,20 +45,99 @@ function showArticles() {
         success: function (response) {
             for(let i=0; i<response.length; i++) {
                 let article = response[i];
-                let articleHTML = addHTML(article);
-                console.log(article);
-                console.log(articleHTML);
+                let id = article.id;
+                let title = article.title;
+                let content = article.content;
+                let updatedAt = article.updatedAt;
+                let articleHTML = addHTML(id, title, content, updatedAt);
                 $('.article-container').append(articleHTML);
             }
         }
     })
+
+    // $(".edit-title").hide();
+    // $(".edit-content").hide();
+    // $(".submit-btn").hide();
+
 }
 
-function addHTML(article) {
+function addHTML(id, title, content, updatedAt) {
     return `
         <div class="article-card">
-            <h2 class="title">제목: ${article.title}</h2>
-            <h1 class="content">내용: ${article.content}</h1>
+            <div id="${id}-updatedAt" class="updatedAt">${updatedAt}</div>
+            제목: <h2 id="${id}-title" class="title">${title}</h2>
+            <input type="text" id="${id}-edit-title" class="edit-title">
+            내용: <h1 id="${id}-content" class="content">${content}</h1>
+            <textarea name="" id="${id}-edit-content" class="edit-content" cols="30" rows="10"></textarea>
+            <button id="${id}-edit-btn" class="edit-btn" onclick="editArticle(${id})">수정하기</button>
+            <button id="${id}-submit-btn" class="submit-btn" onclick="submitArticle(${id})">완료</button>
         </div> 
     `;
+}
+
+function showEdits(id) {
+
+    $(`#${id}-edit-title`).show();
+    $(`#${id}-edit-content`).show();
+    $(`#${id}-submit-btn`).show();
+
+    $(`#${id}-title`).hide();
+    $(`#${id}-content`).hide();
+    $(`#${id}-edit-btn`).hide();
+
+}
+
+function hideEdits(id) {
+
+    $(`#${id}-edit-title`).hide();
+    $(`#${id}-edit-content`).hide();
+    $(`#${id}-submit-btn`).hide();
+
+    $(`#${id}-title`).show();
+    $(`#${id}-content`).show();
+    $(`#${id}-edit-btn`).show();
+
+}
+
+function editArticle(id) {
+
+    showEdits(id);
+
+    let title = $(`#${id}-title`).text().trim();
+    $(`#${id}-edit-title`).val(title);
+    let content = $(`#${id}-content`).text().trim();
+    $(`#${id}-edit-content`).val(content);
+
+}
+
+function submitArticle(id) {
+
+    let title = $(`#${id}-edit-title`).val();
+    if (title=='') {
+        alert("제목을 입력해주세요.");
+        return;
+    }
+
+    let content = $(`#${id}-edit-content`).val();
+    if (content=='') {
+        alert("내용을 입력해주세요.");
+        return;
+    }
+
+    let data = {
+        'title': title,
+        'content': content
+    };
+
+    $.ajax({
+        type: 'PUT',
+        url: `/api/articles/${id}`,
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        success: function (response) {
+            alert("수정이 완료되었습니다!");
+            hideEdits(id);
+            window.location.reload();
+        }
+    });
 }
