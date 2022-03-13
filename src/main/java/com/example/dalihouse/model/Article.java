b23a4e63451caa7eebcfa39dfd0ebb4f4cb8e217
SMJin <sstaw@naver.com>
2022-03-13 19:11:35 +0900

    add JPA class

---
+++ src/main/java/com/example/dalihouse/model/Article.java
@@ -0,0 +1,21 @@
+package com.example.dalihouse.model;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+public class Article extends Timestamped {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String contents;
+
+}
