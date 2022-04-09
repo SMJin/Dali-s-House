96fa12e817393d3ad111b9e56098d64a35d8b9aa
SMJin <sstaw@naver.com>
2022-04-09 22:26:11 +0900

    front-end 페이지 나누기

---
+++ src/main/react-frontend/src/pages/Adoption.js
@@ -0,0 +1,9 @@
+const Adoption = () => {
+    return (
+        <div>
+            <h1>입양 페이지</h1>
+        </div>
+    );
+}
+
+export default Adoption;
---
+++ src/main/react-frontend/src/pages/VolunteerActivity.js
@@ -0,0 +1,9 @@
+const VolunteerActivity = () => {
+    return (
+        <div>
+            <h1>봉사활동 페이지</h1>
+        </div>
+    );
+}
+
+export default VolunteerActivity;
