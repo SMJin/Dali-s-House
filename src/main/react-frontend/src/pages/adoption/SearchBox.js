713662a3ef39c16e78f2e9a9ed271c0fc6750ca9
SMJin <sstaw@naver.com>
2022-04-10 17:55:47 +0900

    ADD react-frontend > Adoption > SearchBox

---
+++ src/main/react-frontend/src/pages/adoption/SearchBox.js
@@ -0,0 +1,18 @@
+import MyImage from "../../components/MyImage";
+import "./css/SearchBox.css"
+
+const SearchBox = () => {
+    return (
+        <div className="search_box">
+            <input
+                className="search_input"
+            />
+            <MyImage
+                type={"icon_search"}
+                imgUrl={"/assets/search_glass.png"}
+            />
+        </div>
+    );
+}
+
+export default SearchBox;
