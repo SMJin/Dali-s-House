4dcf4478a004767c2f8435148a03d42450186313
SMJin <sstaw@naver.com>
2022-04-10 15:10:59 +0900

    react-frontend > Adoption Details page 추가

--- src/main/react-frontend/src/pages/adoption/AdoptionDetails.js
+++ src/main/react-frontend/src/pages/adoption/AdoptionDetails.js
@@ -1,4 +1,6 @@
 import {useParams} from "react-router-dom";
+import MyHeader from "../../components/MyHeader";
+import CrudButton from "../../components/CrudButton";
 
 const AdoptionDetails = () => {
 
@@ -6,7 +8,27 @@ const AdoptionDetails = () => {
     console.log(`입력된 id : ${id}`);
 
     return (
-      <div>입양 상세 페이지입니다.</div>
+        <div>
+            <MyHeader />
+            <h3>입양 상세 페이지입니다.</h3>
+            <div>
+                <CrudButton
+                    text={'제출하기'}
+                    type={'submit'}
+                    onClick={() => alert("제출되었습니다.")}
+                />
+                <CrudButton
+                    text={'수정하기'}
+                    type={'edit'}
+                    onClick={() => alert("수정되었습니다.")}
+                />
+                <CrudButton
+                    text={'삭제하기'}
+                    type={'delete'}
+                    onClick={() => alert("삭제되었습니다.")}
+                />
+            </div>
+        </div>
     );
 }
 
