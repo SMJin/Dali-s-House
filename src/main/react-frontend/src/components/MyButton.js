c671f992c2af8eccb53975a6629ca6f542e1d37d
SMJin <sstaw@naver.com>
2022-04-10 15:26:32 +0900

    ADD react-frontend > Component > MyButton

---
+++ src/main/react-frontend/src/components/MyButton.js
@@ -0,0 +1,22 @@
+import "./css/MyButton.css";
+
+const MyButton = ({text, type, onClick}) => {
+
+    const btnType =  ['head_menus', 'head_logo', 'head_login'].includes(type) ? type : 'default';
+
+    return (
+        <button className={["MyButton", `MyButton_${btnType}`].join(" ")} onClick={onClick}>
+            {text}
+        </button>
+    );
+}
+
+MyButton.defaultProps = {
+    text : 'MyButton',
+    type : 'default',
+    onClick : () => {
+        alert('MyButton 이 눌렸습니다!');
+    }
+};
+
+export default MyButton;
