f1ec28862f1f52c0a18b8b06b7777a4170cfad70
yoojiin <jiinii0522@gmail.com>
2022-04-13 16:15:06 +0900

    C:\Users\JIIN\Dali-s-House\src\main\react-frontend

--- src/main/react-frontend/src/components/MyFooter.js
+++ src/main/react-frontend/src/components/MyFooter.js
@@ -1,23 +1,36 @@
-import "./css/MyFooter.css"
+import "./css/MyFooter.css";
 import MyButton from "./MyButton";
+import { useNavigate } from "react-router-dom";
 
 const MyFooter = () => {
-    return (
-        <div className="MyFooter">
-            <div className="MyFooter_leftText">
-                <h5>봉사활동 | 입양연결 | 커뮤니티</h5>
-                <div><b>달리네 집</b> 경기도 용인시 수지구 죽전로 1F</div>
-                <div><b>Tel.</b> 010 - 1234 - 5678</div>
-                <div><b>후원계좌.</b> 국민 111 - 0000 - 0000 -11 사단법인 달리네 집</div>
-            </div>
-            <div className="MyFooter_rightText">
-                <MyButton text={"달리네 집"} type={"head_logo"} />
-                <div>@DalisHouse</div>
-                <div>ⓒ DalisHouse.2022.All rights reserved</div>
-            </div>
-            <div className="MyFooter_finishLine"> </div>
+  const navigate = useNavigate();
+
+  const goMain = () => {
+    navigate(`/`);
+  };
+
+  return (
+    <div className="MyFooter">
+      <div className="MyFooter_leftText">
+        <h5>봉사활동 | 입양연결 | 커뮤니티</h5>
+        <div>
+          <b>달리네집</b> 경기도 용인시 수지구 죽전로 1F
         </div>
-    );
-}
+        <div>
+          <b>Tel.</b> 010 - 1234 - 5678
+        </div>
+        <div>
+          <b>후원계좌.</b> 국민 111 - 0000 - 0000 -11 사단법인 달리네집
+        </div>
+      </div>
+      <div className="MyFooter_rightText">
+        <MyButton text={"달리네집"} type={"head_logo"} onClick={goMain} />
+        <div>@ DalisHouse</div>
+        <div>ⓒ DalisHouse.2022.All rights reserved</div>
+      </div>
+      <div className="MyFooter_finishLine"> </div>
+    </div>
+  );
+};
 
 export default MyFooter;
