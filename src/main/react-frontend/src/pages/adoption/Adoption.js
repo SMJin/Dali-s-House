b023fe131e1825c4257b6710a3672672c4e3f082
yoojiin <jiinii0522@gmail.com>
2022-04-14 13:17:32 +0900

    C:\Users\JIIN\Dali-s-House\src\main\react-frontend

--- src/main/react-frontend/src/pages/adoption/Adoption.js
+++ src/main/react-frontend/src/pages/adoption/Adoption.js
@@ -1,15 +1,11 @@
-<<<<<<< HEAD
 import "./css/Adoption.css";
-=======
-import './css/Adoption.css';
->>>>>>> master
 import MyHeader from "../../components/MyHeader";
+import MyFooter from "../../components/MyFooter";
 import MyImage from "../../components/MyImage";
 import SearchBox from "./SearchBox";
 import AdoptionThumbnail from "./AdoptionThumbnail";
 
 const Adoption = () => {
-<<<<<<< HEAD
   return (
     <div>
       <h1>입양 페이지</h1>
@@ -76,81 +72,9 @@ const Adoption = () => {
           identification={true}
         />
       </div>
+      <MyFooter />
     </div>
   );
 };
 
 export default Adoption;
-=======
-    return (
-        <div>
-            <h1>입양 페이지</h1>
-            <MyHeader />
-            <h3>입양연결</h3>
-            <div className="adoption_best">
-                <MyImage type={"img_best"} />
-                <div className="adoption_best_content">
-                    <h3>달리에게 가족이 생겼어요!</h3>
-                    <h5>베스트 입양 후기 글</h5>
-                </div>
-            </div>
-            <SearchBox />
-            <div className="menu" >
-                <h5>종류 성별 중성화 지역</h5>
-            </div>
-            <div className="adoption_thumbnail_board">
-                <AdoptionThumbnail
-                    type={"dog"}
-                    name={"달리"}
-                    address={"경기도 용인시"}
-                    sex={"남아"}
-                    neutering={true}
-                    identification={true}
-                />
-                <AdoptionThumbnail
-                    type={"dog"}
-                    name={"달리"}
-                    address={"경기도 용인시"}
-                    sex={"남아"}
-                    neutering={true}
-                    identification={true}
-                />
-                <AdoptionThumbnail
-                    type={"dog"}
-                    name={"달리"}
-                    address={"경기도 용인시"}
-                    sex={"남아"}
-                    neutering={true}
-                    identification={true}
-                />
-                <AdoptionThumbnail
-                    type={"dog"}
-                    name={"달리"}
-                    address={"경기도 용인시"}
-                    sex={"남아"}
-                    neutering={true}
-                    identification={true}
-                />
-                <AdoptionThumbnail
-                    type={"dog"}
-                    name={"달리"}
-                    address={"경기도 용인시"}
-                    sex={"남아"}
-                    neutering={true}
-                    identification={true}
-                />
-                <AdoptionThumbnail
-                    type={"dog"}
-                    name={"달리"}
-                    address={"경기도 용인시"}
-                    sex={"남아"}
-                    neutering={true}
-                    identification={true}
-                />
-            </div>
-        </div>
-    );
-}
-
-export default Adoption;
->>>>>>> master
