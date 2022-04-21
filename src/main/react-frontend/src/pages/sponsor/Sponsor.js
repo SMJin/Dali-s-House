50ecc98f6da75cd494adbeb6158c4ea5d8ded0cf
yoojiin <jiinii0522@gmail.com>
2022-04-22 03:44:36 +0900

    후원페이지,매거진페이지 수정

--- src/main/react-frontend/src/pages/sponsor/Sponsor.js
+++ src/main/react-frontend/src/pages/sponsor/Sponsor.js
@@ -1,13 +1,25 @@
+import MyFooter from "../../components/MyFooter";
 import MyHeader from "../../components/MyHeader";
+import MyPagination from "../../components/MyPagination";
 import "./css/Sponsor.css";
 
-const Sponsor = () => {
+const Sponsor = ({ sponsorList }) => {
   return (
     <div className="Sponsor">
       <h1>후원 페이지</h1>
       <MyHeader />
+      <h3>후원 리스트</h3>
+      <div className="Sponsor_box">
+        <h6>{sponsorList.length} 개의 게시물이 있습니다.</h6>
+      </div>
+      <MyPagination />
+      <MyFooter />
     </div>
   );
+};
+
+Sponsor.defaultProps = {
+  sponsorList: [],
 };
 
 export default Sponsor;
