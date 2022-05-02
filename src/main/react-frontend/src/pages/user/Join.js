4fc58387c9e15f9c34c0fc988fcbf1e2f06bf932
yoojiin <jiinii0522@gmail.com>
2022-05-03 00:57:30 +0900

    회원가입 페이지 입력 유효성 확인 기능 완성

--- src/main/react-frontend/src/pages/user/Join.js
+++ src/main/react-frontend/src/pages/user/Join.js
@@ -1,172 +1,139 @@
-import React, { useState } from "react";
-
-import MyFooter from "../../components/MyFooter";
-import MyHeader from "../../components/MyHeader";
 import "./css/Join.css";
 
+import { useNavigate } from "react-router-dom";
+import React, { useCallback, useState } from "react";
+import MyHeader from "../../components/MyHeader";
+import MyFooter from "../../components/MyFooter";
+
 const Join = () => {
+  const navigate = useNavigate();
   const goJoin = () => {
-    if (id.length == 0) {
-      alert("아이디를 입력해주세요.");
-    } else if (password.length == 0) {
-      alert("비밀번호를 입력해주세요.");
-    } else if (name.length == 0) {
-      alert("이름을 입력해주세요.");
-    } else if (email.length == 0) {
-      alert("이메일을 입력해주세요.");
-    } else if (number.length == 0) {
-      alert("휴대폰 번호를 입력해주세요.");
-    } else if (`${idValid}` == 0) {
-      alert("아이디를 올바르게 입력해주세요.");
-    } else if (`${passwordValid}` == 0) {
-      alert("비밀번호를 올바르게 입력해주세요.");
-    } else if (`${nameValid}` == 0) {
-      alert("이름을 올바르게 입력해주세요.");
-    } else if (`${emailValid}` == 0) {
-      alert("이메일을 올바르게 입력해주세요.");
-    } else if (`${agree}` == 0) {
-      alert("개인정보 수집 및 이용 동의에 동의해주세요.");
-    } else if (`${numberValid}` == 0) {
-      alert("휴대폰 번호를 올바르게 입력해주세요.");
-    } else {
-      alert("가입이 완료 되었습니다!");
-    }
-  };
-
-  const goConfirmRepetition = () => {
-    alert("사용 가능합니다");
-  };
-
-  const goVerificationCode = () => {
-    alert("인증번호가 발송되었습니다");
-  };
-
-  const goFindAddress = () => {
-    alert("주소 찾기");
+    alert("가입이 완료되었습니다!");
+    navigate(`/`);
   };
 
   const [id, setId] = useState("");
-  const [idValid, setIdValid] = useState("");
-
-  const checkId = (e) => {
-    const regex = /^[a-z|A-Z|0-9\b]{0,10}$/;
-
-    if (regex.test(e.target.value)) {
-      setId(e.target.value);
-    }
-  };
-
-  const IsID = (e) => {
-    if (id.length < 5) {
-      alert("너무 짧습니다.");
-      setId("");
-      setIdValid(0);
-    } else {
-      setIdValid(1);
-    }
-  };
-
   const [password, setPassword] = useState("");
-  const [passwordValid, setPasswordValid] = useState(0);
-
-  const checkPassword = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const IsPassword = () => {
-    if (password.length < 5) {
-      alert("너무 짧습니다.");
-      setPasswordValid(0);
-    } else {
-      setPasswordValid(1);
-    }
-  };
-
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [confirmpasswordValid, setConfirmPasswordValid] = useState(0);
-
-  const checkConfirmPassword = (e) => {
-    setConfirmPassword(e.target.value);
-  };
-
-  const IsConfirmPassword = () => {
-    if (password != confirmPassword) {
-      alert("비밀번호가 일치하지 않습니다.");
-      setConfirmPassword("");
-      setConfirmPasswordValid(0);
-    } else {
-      setConfirmPasswordValid(1);
-    }
-  };
-
+  const [passwordConfirm, setPasswordConfirm] = useState("");
   const [name, setName] = useState("");
-  const [nameValid, setNameValid] = useState("");
-
-  const checkName = (e) => {
-    const regex = /^[ㄱ-ㅎ|가-힣\b]{0,5}$/;
-
-    if (regex.test(e.target.value)) {
-      setName(e.target.value);
-    }
-  };
-
-  const IsName = (e) => {
-    var regExp = /^[가-힣]{0,5}$/;
-    if (!regExp.test(e.target.value)) {
-      alert("단어 형식으로 입력해주세요");
-      setName("");
-      setNameValid(0);
-    } else {
-      setNameValid(1);
-    }
-  };
-
   const [number, setNumber] = useState("");
-  const [numberValid, setNumberValid] = useState(0);
-
-  const checkNumber = (e) => {
-    const regex = /^[0-9\b]{0,11}$/;
-
-    if (regex.test(e.target.value)) {
-      setNumber(e.target.value);
-    }
-  };
-
-  const IsNumber = (e) => {
-    if (number.length < 10) {
-      alert("번호가 너무 짧습니다. 확인 후 다시 입력해주세요.");
-      setNumberValid(0);
-    } else {
-      setNumberValid(1);
-    }
-  };
-
   const [email, setEmail] = useState("");
-  const [emailValid, setEmailValid] = useState(0);
+  const [agree, setAgree] = useState(false);
+  const [count, setCount] = useState(1);
 
-  const checkEmail = (e) => {
-    const regex = /^[0-9|a-z|A-Z|@|.\b]{0,30}$/;
+  //오류메시지 상태저장
+  const [idMessage, setIdMessage] = useState("");
+  const [passwordMessage, setPasswordMessage] = useState("");
+  const [passwordConfirmMessage, setPasswordConfirmMessage] = useState("");
+  const [nameMessage, setNameMessage] = useState("");
+  const [numberMessage, setNumberMessage] = useState("");
+  const [emailMessage, setEmailMessage] = useState("");
 
-    if (regex.test(e.target.value)) {
-      setEmail(e.target.value);
-    }
-  };
+  //유효성 검사
+  const [IsId, setIsId] = useState(false);
+  const [IsPassword, setIsPassword] = useState(false);
+  const [IsPasswordConfirm, setIsPasswordConfirm] = useState(false);
+  const [IsName, setIsName] = useState(false);
+  const [IsNumber, setIsNumber] = useState(false);
+  const [IsEmail, setIsEmail] = useState(false);
 
-  const IsEmail = (e) => {
-    var regExp =
-      /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
-    if (!regExp.test(e.target.value)) {
-      alert("이메일 형식이 올바르지 않습니다.");
-      setEmailValid(0);
+  const onChangeId = useCallback((e) => {
+    const idRegex = /^[a-z|A-Z|0-9\b]{5,10}$/;
+    const idCurrent = e.target.value;
+    setId(idCurrent);
+    if (!idRegex.test(idCurrent)) {
+      setIdMessage("5글자 이상 입력해주세요!");
+      setIsId(false);
     } else {
-      setEmailValid(1);
+      setIdMessage("올바른 아이디 형식입니다.");
+      setIsId(true);
     }
-  };
+  }, []);
 
-  const [agree, setAgree] = useState(0);
+  const onChangePassword = useCallback((e) => {
+    const passwordRegex =
+      /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/;
+    const passwordCurrent = e.target.value;
+    setPassword(passwordCurrent);
 
-  const checkAgree = () => {
-    setAgree(1);
+    if (!passwordRegex.test(passwordCurrent)) {
+      setPasswordMessage(
+        "숫자+영문자+특수문자 조합으로 8자리 이상 입력해주세요!"
+      );
+      setIsPassword(false);
+    } else {
+      setPasswordMessage("올바른 비밀번호 형식입니다.");
+      setIsPassword(true);
+    }
+  }, []);
+
+  const onChangePasswordConfirm = useCallback(
+    (e) => {
+      const passwordConfirmCurrent = e.target.value;
+      setPasswordConfirm(passwordConfirmCurrent);
+      if (password == passwordConfirmCurrent) {
+        setPasswordConfirmMessage("비밀번호가 일치합니다.");
+        setIsPasswordConfirm(true);
+      } else {
+        setPasswordConfirmMessage(
+          "비밀번호가 일치하지 않습니다. 다시 입력해주세요."
+        );
+        setIsPasswordConfirm(false);
+      }
+    },
+    [password]
+  );
+
+  const onChangeName = useCallback((e) => {
+    const nameRegex = /^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣\b]{0,5}$/;
+    const nameRegex2 = /^[가-힣]{0,5}$/;
+    const nameCurrent = e.target.value;
+    if (nameRegex.test(nameCurrent)) {
+      setName(nameCurrent);
+    }
+    if ((nameCurrent.length < 2) | !nameRegex2.test(nameCurrent)) {
+      setNameMessage("2글자 이상 한글 단어로 입력해주세요.");
+      setIsName(false);
+    } else {
+      setNameMessage("올바른 이름 형식입니다.");
+      setIsName(true);
+    }
+  }, []);
+
+  const onChangeNumber = useCallback((e) => {
+    const numberRegex = /^[0-9\b]{0,11}$/;
+    const numberCurrent = e.target.value;
+    if (numberRegex.test(numberCurrent)) {
+      setNumber(numberCurrent);
+    }
+    if (numberCurrent.length < 10) {
+      setNumberMessage("너무 짧습니다.");
+      setIsNumber(false);
+    } else {
+      setNumberMessage("올바른 휴대폰 번호 형식입니다.");
+      setIsNumber(true);
+    }
+  }, []);
+
+  const onChangeEmail = useCallback((e) => {
+    const emailRegex =
+      /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    const emailCurrent = e.target.value;
+    setEmail(emailCurrent);
+    if (!emailRegex.test(emailCurrent)) {
+      setEmailMessage("이메일 형식이 틀렸습니다. 다시 확인해주세요!");
+      setIsEmail(false);
+    } else {
+      setEmailMessage("올바른 이메일 형식입니다.");
+      setIsEmail(true);
+    }
+  }, []);
+
+  const onChangeAgree = (e) => {
+    if (count % 2 == 1) {
+      setAgree(true);
+    } else setAgree(false);
+    setCount(count + 1);
   };
 
   return (
@@ -176,145 +143,137 @@ const Join = () => {
       <div className="join">
         <h2>회원가입</h2>
         <hr />
-        <div>
-          <div className="join_ID">
-            <div className="left_type">아이디</div>
-            <div className="middle_input">
-              <input
-                className="newID"
-                placeholder="5자 이상 10자 이하의 영문 혹은 숫자를 조합해주세요"
-                type="text"
-                value={id}
-                maxLength="10"
-                onChange={checkId}
-                onBlur={IsID}
-              />
-            </div>
-            <div className="right_button">
-              <button className="GoCRButton" onClick={goConfirmRepetition}>
-                중복확인
-              </button>
-            </div>
-          </div>
-          <div className="join_PW">
-            <div className="left_type">비밀번호</div>
-            <div className="middle_input">
-              <input
-                className="newPW"
-                placeholder="비밀번호를 입력해주세요"
-                type="password"
-                maxLength="15"
-                value={password}
-                onChange={checkPassword}
-                onBlur={IsPassword}
-              />
-            </div>
-            <div className="right_button"></div>
-          </div>
-          <div className="join_PW_confirm">
-            <div className="left_type">비밀번호 확인</div>
-            <div className="middle_input">
-              <input
-                className="confirmPW"
-                placeholder="비밀번호를 한번 더 입력해주세요"
-                type="password"
-                maxLength="15"
-                value={confirmPassword}
-                onChange={checkConfirmPassword}
-                onBlur={IsConfirmPassword}
-              />
-            </div>
-            <div className="right_button"></div>
-          </div>
-          <div className="join_name">
-            <div className="left_type">이름</div>
-            <div className="middle_input">
-              <input
-                className="newName"
-                placeholder="이름을 입력해주세요"
-                type="text"
-                value={name}
-                onChange={checkName}
-                onBlur={IsName}
-              />
-            </div>
-            <div className="right_button"></div>
-          </div>
-          <div className="join_email">
-            <div className="left_type">이메일</div>
-            <div className="middle_input">
-              <input
-                className="newEmail"
-                placeholder="예: dalishouse@naver.com"
-                type="text"
-                value={email}
-                onChange={checkEmail}
-                onBlur={IsEmail}
-              />
-            </div>
-            <div className="right_button">
-              <button className="GoCRButton" onClick={goConfirmRepetition}>
-                중복확인
-              </button>
-            </div>
-          </div>
-          <div className="join_hp">
-            <div className="left_type">휴대폰</div>
-            <div className="middle_input">
-              <input
-                className="newHP"
-                value={number}
-                placeholder="숫자만 입력해주세요"
-                type="text"
-                maxlength="11"
-                onChange={checkNumber}
-                onBlur={IsNumber}
-              />
-            </div>
-            <div className="right_button">
-              <button className="GoVCButton" onClick={goVerificationCode}>
-                인증번호 받기
-              </button>
-            </div>
-          </div>
-          <div className="join_address">
-            <div className="left_type">주소</div>
-            <div className="middle_input">
-              <button className="GoFindADRButton" onClick={goFindAddress}>
-                주소찾기
-              </button>
-            </div>
-            <div className="right_button"></div>
-          </div>
-          <div className="join_sex">
-            <div className="left_type">성별</div>
-            <div className="middle_input">
-              <button className="sex_male">남자</button>
-              <button className="sex_female">여자</button>
-            </div>
-            <div className="right_button"></div>
-          </div>
-          <div className="join_birth">
-            <div className="left_type">생년월일</div>
-            <div className="middle_input">
-              <input className="newBirth" type="date" />
-            </div>
-            <div className="right_button"></div>
-          </div>
+        <div className="join_item">
+          <div className="join_item_title">아이디</div>
+          <input
+            value={id}
+            placeholder=" 10자 이하의 영문 혹은 숫자를 입력해주세요"
+            type="text"
+            maxLength="10"
+            onChange={onChangeId}
+          ></input>
+        </div>
+        <div className="join_item_message">
+          <span
+            className={`${IsId ? "join_input_success" : "join_input_error"}`}
+          >
+            {idMessage}
+          </span>
+        </div>
+        <div className="join_item">
+          <div className="join_item_title">비밀번호</div>
+          <input
+            value={password}
+            placeholder=" 비밀번호를 입력해주세요"
+            type="password"
+            onChange={onChangePassword}
+          ></input>
+        </div>
+        <div className="join_item_message">
+          <span
+            className={`${
+              IsPassword ? "join_input_success" : "join_input_error"
+            }`}
+          >
+            {passwordMessage}
+          </span>
+        </div>
+        <div className="join_item">
+          <div className="join_item_title">비밀번호 확인</div>
+          <input
+            value={passwordConfirm}
+            placeholder=" 비밀번호를 한번 더 입력해주세요"
+            type="password"
+            onChange={onChangePasswordConfirm}
+          ></input>
+        </div>
+        <div className="join_item_message">
+          <span
+            className={`${
+              IsPasswordConfirm ? "join_input_success" : "join_input_error"
+            }`}
+          >
+            {passwordConfirmMessage}
+          </span>
+        </div>
+        <div className="join_item">
+          <div className="join_item_title">이름</div>
+          <input
+            value={name}
+            placeholder=" 이름을 입력해주세요"
+            type="text"
+            maxLength="5"
+            onChange={onChangeName}
+          ></input>
+        </div>
+        <div className="join_item_message">
+          <span
+            className={`${IsName ? "join_input_success" : "join_input_error"}`}
+          >
+            {nameMessage}
+          </span>
+        </div>
+        <div className="join_item">
+          <div className="join_item_title">휴대폰</div>
+          <input
+            value={number}
+            placeholder=" 숫자만 입력해주세요"
+            type="text"
+            onChange={onChangeNumber}
+          ></input>
+        </div>
+        <div className="join_item_message">
+          <span
+            className={`${
+              IsNumber ? "join_input_success" : "join_input_error"
+            }`}
+          >
+            {numberMessage}
+          </span>
+        </div>
+        <div className="join_item">
+          <div className="join_item_title">이메일</div>
+          <input
+            value={email}
+            placeholder=" 예: dalishouse@naver.com"
+            type="text"
+            onChange={onChangeEmail}
+          ></input>
+        </div>
+        <div className="join_item_message">
+          <span
+            className={`${IsEmail ? "join_input_success" : "join_input_error"}`}
+          >
+            {emailMessage}
+          </span>
+        </div>
+        <div className="join_privacyCheck">
+          <input type={"checkbox"} value={agree} onChange={onChangeAgree} />
+          <label> 개인정보 수집 및 이용 동의</label>
         </div>
         <div>
-          <input type={"checkbox"} value={agree} onClick={checkAgree} />
-          <label>개인정보 수집 및 이용 동의</label>
-        </div>
-        <div>
-          <button className="GoJoinButton" onClick={goJoin}>
-            가입하기
-          </button>
+          <input
+            className="join_submitButton"
+            type="button"
+            value="가입하기"
+            id="join_button"
+            disabled={
+              !(
+                IsId &&
+                IsPassword &&
+                IsPasswordConfirm &&
+                IsName &&
+                IsNumber &&
+                IsEmail &&
+                agree
+              )
+            }
+            onClick={goJoin}
+          ></input>
         </div>
       </div>
       <MyFooter />
     </div>
   );
 };
-
 export default Join;
