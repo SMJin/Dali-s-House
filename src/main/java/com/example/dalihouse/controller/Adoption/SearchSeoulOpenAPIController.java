ac2b0087edf2962eef314eeb199afa60f23072d4
SMJin <sstaw@naver.com>
2022-05-04 03:56:56 +0900

    edit adoptionThumbnail frontend axios

--- src/main/java/com/example/dalihouse/controller/Adoption/SearchSeoulOpenAPIController.java
+++ src/main/java/com/example/dalihouse/controller/Adoption/SearchSeoulOpenAPIController.java
@@ -4,6 +4,7 @@ import com.example.dalihouse.dto.Adoption.AdoptionThumbnailDto;
 import com.example.dalihouse.utils.SeoulOpenApi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +16,8 @@ public class SearchSeoulOpenAPIController {
 
     private final SeoulOpenApi seoulOpenApi;
 
-    @GetMapping("/api/adoptionThumbnails")
-    public List<AdoptionThumbnailDto> getAdoptionThumbnail(@RequestParam int startIdx, @RequestParam int endIdx) {
+    @GetMapping("/api/adoptionThumbnails/{startIdx}/{endIdx}")
+    public List<AdoptionThumbnailDto> getAdoptionThumbnail(@PathVariable int startIdx, @PathVariable int endIdx) {
         String result = seoulOpenApi.search(startIdx, endIdx);
         return seoulOpenApi.fromJSONtoItems(result);
     }
