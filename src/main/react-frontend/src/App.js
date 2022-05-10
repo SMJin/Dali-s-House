2acbfbbf261690c6c001ea6f0da3f38da885087f
yoojiin <jiinii0522@gmail.com>
2022-05-10 14:01:56 +0900

    axios:: join/login-frontend

--- src/main/react-frontend/src/App.js
+++ src/main/react-frontend/src/App.js
@@ -54,6 +54,39 @@ const reducer = (state, action) => {
 export const CommunityStateContext = React.createContext();
 export const CommunityDispatchContext = React.createContext();
 
+// 수정 시작
+const reducerUser = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      newState = [action.data, ...state];
+      break;
+    }
+    // case "REMOVE": {
+    //   newState = state.filter((it) => it.id !== action.targetId);
+    //   break;
+    // }
+    // case "EDIT": {
+    //   newState = state.map((it) =>
+    //     it.id === action.data.id ? { ...action.data } : it
+    //   );
+    //   break;
+    // }
+    default:
+      return state;
+  }
+
+  localStorage.setItem("user", JSON.stringify(newState));
+  return newState;
+};
+
+export const UserStateContext = React.createContext();
+export const UserDispatchContext = React.createContext();
+// 수정 끝
+
 function App() {
   const [data, dispatch] = useReducer(reducer, []);
 
@@ -71,8 +104,8 @@ function App() {
     }
   }, []);
 
-  const dataId = useRef(1);
-  //const dataId = useRef(data.length + 1);
+  // const dataId = useRef(1);
+  const dataId = useRef(data.length + 1);
 
   // CREATE
   const onCreate = (date, category, title, content) => {
@@ -93,12 +126,13 @@ function App() {
     dispatch({ type: "REMOVE", targetId });
   };
   // EDIT
-  const onEdit = (targetId, date, category, title, content) => {
+  const onEdit = (targetId, /*targetDate*/ date, category, title, content) => {
     dispatch({
       type: "EDIT",
       data: {
         id: targetId,
         date: new Date(date).getTime(),
+        // date: targetDate
         category,
         title,
         content,
@@ -106,62 +140,104 @@ function App() {
     });
   };
 
+  // 수정 시작
+  const [userData, userDispatch] = useReducer(reducerUser, []);
+
+  useEffect(() => {
+    const localData = localStorage.getItem("user");
+    if (localData) {
+      const userList = JSON.parse(localData).sort(
+        (a, b) => parseInt(b.id) - parseInt(a.id)
+      );
+
+      if (userList.length >= 1) {
+        UserDataId.current = parseInt(userList[0].id) + 1;
+        userDispatch({ type: "INIT", data: userList });
+      }
+    }
+  }, []);
+
+  const UserDataId = useRef(userData.length + 1);
+
+  // CREATE
+  const onUserCreate = (id, password, name, number, email) => {
+    userDispatch({
+      type: "CREATE",
+      data: {
+        id,
+        password,
+        name,
+        number,
+        email,
+      },
+    });
+    UserDataId.current += 1;
+  };
+  // 수정 끝
+
   return (
-    <CommunityStateContext.Provider value={data}>
-      <CommunityDispatchContext.Provider
-        value={{
-          onCreate,
-          onEdit,
-          onRemove,
-        }}
-      >
-        <BrowserRouter>
-          <div className="App">
-            <h1>달리네 집 App.js</h1>
-            <Routes>
-              <Route path="/" element={<Main />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/join" element={<Join />} />
-              <Route path="/find/id" element={<FindID />} />
-              <Route path="/find/password" element={<FindPW />} />
-              <Route path="/community" element={<Community />} />
-              <Route path="/community/new" element={<Community_New />} />
-              <Route path="/community/edit/:id" element={<Community_Edit />} />
-              <Route path="/community/:id" element={<Community_Detail />} />
-              <Route path="/adoption" element={<Adoption />} />
-              <Route
-                path="/adoption/thumbnail/edit"
-                element={<AdoptionThumbnailEdit />}
-              />
-              <Route
-                path="/adoption/edit"
-                element={<AdoptionEdit onCreate={null} />}
-              />
-              <Route
-                path="/volunteerActivity"
-                element={<VolunteerActivity />}
-              />
-              <Route
-                path="/volunteerActivity/thumbnail/edit"
-                element={<VolunteerActivityThumbnailEdit />}
-              />
-              <Route
-                path="/volunteerActivity/edit"
-                element={<VolunteerActivityEdit />}
-              />
-              <Route path="/myPage" element={<MyPage />} />
-              <Route path="/myVolunteer" element={<MyVolunteer />} />
-              <Route
-                path="/myFavoriteVolunteer"
-                element={<MyFavoriteVolunteer />}
-              />
-              <Route path="/community/magazine" element={<Magazine />} />
-              <Route path="/sponsor" element={<Sponsor />} />
-            </Routes>
-          </div>
-        </BrowserRouter>
-      </CommunityDispatchContext.Provider>
-    </CommunityStateContext.Provider>
+    <UserStateContext.Provider value={userData}>
+      <UserDispatchContext.Provider value={{ onUserCreate }}>
+        <CommunityStateContext.Provider value={data}>
+          <CommunityDispatchContext.Provider
+            value={{
+              onCreate,
+              onEdit,
+              onRemove,
+            }}
+          >
+            <BrowserRouter>
+              <div className="App">
+                <h1>달리네 집 App.js</h1>
+                <Routes>
+                  <Route path="/" element={<Main />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/join" element={<Join />} />
+                  <Route path="/find/id" element={<FindID />} />
+                  <Route path="/find/password" element={<FindPW />} />
+                  <Route path="/community" element={<Community />} />
+                  <Route path="/community/new" element={<Community_New />} />
+                  <Route
+                    path="/community/edit/:id"
+                    element={<Community_Edit />}
+                  />
+                  <Route path="/community/:id" element={<Community_Detail />} />
+                  <Route path="/adoption" element={<Adoption />} />
+                  <Route
+                    path="/adoption/thumbnail/edit"
+                    element={<AdoptionThumbnailEdit />}
+                  />
+                  <Route
+                    path="/adoption/edit"
+                    element={<AdoptionEdit onCreate={null} />}
+                  />
+                  <Route
+                    path="/volunteerActivity"
+                    element={<VolunteerActivity />}
+                  />
+                  <Route
+                    path="/volunteerActivity/thumbnail/edit"
+                    element={<VolunteerActivityThumbnailEdit />}
+                  />
+                  <Route
+                    path="/volunteerActivity/edit"
+                    element={<VolunteerActivityEdit />}
+                  />
+                  <Route path="/myPage" element={<MyPage />} />
+                  <Route path="/myVolunteer" element={<MyVolunteer />} />
+                  <Route
+                    path="/myFavoriteVolunteer"
+                    element={<MyFavoriteVolunteer />}
+                  />
+                  <Route path="/community/magazine" element={<Magazine />} />
+                  <Route path="/sponsor" element={<Sponsor />} />
+                </Routes>
+              </div>
+            </BrowserRouter>
+          </CommunityDispatchContext.Provider>
+        </CommunityStateContext.Provider>
+      </UserDispatchContext.Provider>
+    </UserStateContext.Provider>
   );
 }
 
