0294ba1ee744700b630e23182cb1e076ac16d999
yoojiin <jiinii0522@gmail.com>
2022-05-10 22:49:08 +0900

    헤더수정

--- src/main/react-frontend/src/pages/Mypage/MyPage.js
+++ src/main/react-frontend/src/pages/Mypage/MyPage.js
@@ -4,36 +4,32 @@ import "./css/MyPage.css";
 import MyPageHeader from "./MyPageHeader";
 import MyPageMenu from "./MyPageMenu";
 
-
 const MyPage = () => {
-    
-
-
-return (
-    <div className = "Mypage">
-        <div className = "mypage_header">
+  return (
+    <div className="Mypage">
+      <div className="mypage_header">
         <h1>마이페이지</h1>
         <MyPageHeader />
-        <hr/>
-    </div>
+        <hr />
+      </div>
 
-        <div className="my_volunteer"><br />
-        
+      <div className="my_volunteer">
+        <br />
+
         <SearchBox className="searchbox" />
-        <MyPageMenu/>
-
-        </div>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>  
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <MyFooter />
+        <MyPageMenu />
+      </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <MyFooter />
     </div>
-    );
+  );
 };
 
 export default MyPage;
--- src/main/react-frontend/src/pages/Mypage/MyPageMenu.js
+++ src/main/react-frontend/src/pages/Mypage/MyPageMenu.js
@@ -1,24 +1,37 @@
 import "./css/MyPageMenu.css";
 
 const MyPageMenu = () => {
-
-return (
-<div class="mypage_menubar">
-<ul>
-  <li><a href="#" id="current">봉사 활동</a>
-   <ul>
-     <li><a href="MyVolunteer">나의 봉사</a></li>
-     <li><a href="MyFavoriteVolunteer">찜한 봉사</a></li>
-     <li><a href="#">봉사인증서 발급</a></li>
-    </ul>
- </li>
- <li><a href="#">후원 기부</a></li>
- <li><a href="#">설정</a></li>
- <li><a href="#">공지사항</a></li>
-</ul>
-</div>
-
- );
-}
+  return (
+    <div class="mypage_menubar">
+      <ul>
+        <li>
+          <a href="#" id="current">
+            봉사 활동
+          </a>
+          <ul>
+            <li>
+              <a href="MyVolunteer">나의 봉사</a>
+            </li>
+            <li>
+              <a href="MyFavoriteVolunteer">찜한 봉사</a>
+            </li>
+            <li>
+              <a href="#">봉사인증서 발급</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">후원 기부</a>
+        </li>
+        <li>
+          <a href="#">설정</a>
+        </li>
+        <li>
+          <a href="#">공지사항</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default MyPageMenu;
