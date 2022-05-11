1275cd9fd7db0ed91426063393d69a0137434886
yoojiin <jiinii0522@gmail.com>
2022-05-11 16:52:59 +0900

    로그인 예외처리 및 로그인시 로컬스토리지 이용

--- src/main/react-frontend/src/pages/Mypage/MyPage.js
+++ src/main/react-frontend/src/pages/Mypage/MyPage.js
@@ -4,21 +4,18 @@ import MyHeader from "../../components/MyHeader";
 import MyPageMenu from "./MyPageMenu";
 
 const MyPage = () => {
-
-return (
-    <div className = "Mypage">
-        <h1>마이페이지</h1>
-        <MyHeader />
-        <div className="mypage_wrapper">
-        <span><b>마이페이지</b></span>
+  return (
+    <div className="Mypage">
+      <h1>마이페이지</h1>
+      <MyHeader />
+      <div className="mypage_wrapper">
+        <span>
+          <b>마이페이지</b>
+        </span>
         <MyPageMenu />
+      </div>
 
-
-        
-        </div>
-
-        <MyFooter />
-
+      <MyFooter />
     </div>
   );
 };
