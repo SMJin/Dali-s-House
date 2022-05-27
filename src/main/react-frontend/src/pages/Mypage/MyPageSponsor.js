7705246f46f3c68bbf971292b91823b98c68356e
yoojiin <jiinii0522@gmail.com>
2022-05-27 21:26:01 +0900

    마이페이지 메뉴 수정

--- src/main/react-frontend/src/pages/Mypage/MyPageSponsor.js
+++ src/main/react-frontend/src/pages/Mypage/MyPageSponsor.js
@@ -5,11 +5,33 @@ import MyHeader from "../../components/MyHeader";
 import MyPageMenu from "./MyPageMenu";
 import MyFooter from "../../components/MyFooter";
 import MySearch from "../../components/MySearch";
-import "./css/MyPageSponsor.css";
+import MyPageMenuTest from "../../components/MyPageMenu";
+import MyPagination from "../../components/MyPagination";
+// import "./css/MyPageSponsor.css";
 
-const MyPageSponsor = (props) => {
+const MyPageSponsor = () => {
   return (
     <div className="MyPageSponsor">
+      <MyHeader />
+      <div className="mypageWrapper">
+        <MyPageMenuTest clicked={"spon"} />
+        <div className="mypageContentWrapper">
+          <b>후원 내역</b>
+
+          <SponsorList />
+          <MyPagination />
+        </div>
+      </div>
+
+      <MyFooter />
+    </div>
+  );
+};
+
+export default MyPageSponsor;
+
+/*
+<div className="MyPageSponsor">
       <MyHeader />
 
       <MySearch />
@@ -19,7 +41,4 @@ const MyPageSponsor = (props) => {
 
       <SponsorList />
     </div>
-  );
-};
-
-export default MyPageSponsor;
+*/
