f2e7b9bfaccea3a1dc4b00861d189e9a2ad9cbc9
yoojiin <jiinii0522@gmail.com>
2022-05-27 15:11:30 +0900

    마이페이지 봉사리스트 수정

--- src/main/react-frontend/src/pages/Mypage/MyVolunteer.js
+++ src/main/react-frontend/src/pages/Mypage/MyVolunteer.js
@@ -1,10 +1,10 @@
 import MyHeader from "../../components/MyHeader";
 import MyFooter from "../../components/MyFooter";
-import MyVolunteerList from "./MyVolunteerList";
 import "./css/MyVolunteer.css";
 import MyPageMenu from "./MyPageMenu";
 import MyPagination from "../../components/MyPagination";
 import MySearch from "../../components/MySearch";
+import { certList } from "./CertDataList";
 
 const MyVolunteer = () => {
   return (
@@ -19,12 +19,27 @@ const MyVolunteer = () => {
       </div>
       <div>
         <div className="mypage_myvolunteer_list">
-          <MyVolunteerList />
-          <MyVolunteerList />
-          <MyVolunteerList />
-          <MyVolunteerList />
-          <MyVolunteerList />
-          <MyVolunteerList />
+          {certList.map((it) => (
+            <div className="mypageVolunteerItemWapper" id={it.id}>
+              <div className="mypageVolunteerImgWrapper">
+                <img
+                  className="mypageVolunteerImg"
+                  src={
+                    process.env.PUBLIC_URL +
+                    `./assets/volunteer/volunteer_${(it.id % 8) + 1}.png`
+                  }
+                />
+              </div>
+              <div className="mypageVolunteerTextWapper">
+                <div className="mypageVolunteerTitle">{it.title}</div>
+                <div className="mypageVolunteerOrgan">{it.organ}</div>
+                <div className="mypageVolunteerDate">
+                  {it.date} {it.time}
+                </div>
+                <div className="mypageVolunteerContent">상세: {it.content}</div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
       <MyPagination />
