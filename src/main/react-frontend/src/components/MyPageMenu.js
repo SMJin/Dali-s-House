2bb00a3a0bcadc02b039a97b0a44798e2d2ff631
yoojiin <87603329+yoojiin@users.noreply.github.com>
2022-05-27 22:15:39 +0900

    Update MyPageMenu.js

--- src/main/react-frontend/src/components/MyPageMenu.js
+++ src/main/react-frontend/src/components/MyPageMenu.js
@@ -8,21 +8,56 @@ const MyPageMenuTest = ({ clicked }) => {
   const [isVolunteer, setIsVolunteer] = useState(false);
   const [isSpon, setIsSpon] = useState(false);
   const [isSet, setIsSet] = useState(false);
+  const [isNotice, setIsNotice] = useState(false);
+
+  const [isClick_vol, setIsClick_vol] = useState(0);
+  const [isClick_spon, setIsClick_spon] = useState(0);
+  const [isClick_set, setIsClick_set] = useState(0);
+  const [isClick_notice, setIsClick_notice] = useState(0);
 
   useEffect(() => {
-    if (clicked == "volunteer") {
+    if (clicked == "volunteer" || isClick_vol % 2 == 1) {
       setIsVolunteer(true);
-    } else if (clicked == "spon") {
+    } else {
+      setIsVolunteer(false);
+    }
+    if (clicked == "spon" || isClick_spon % 2 == 1) {
       setIsSpon(true);
-    } else if (clicked == "set") {
+    } else {
+      setIsSpon(false);
+    }
+    if (clicked == "set" || isClick_set % 2 == 1) {
       setIsSet(true);
+    } else {
+      setIsSet(false);
+    }
+    if (clicked == "set" || isClick_notice % 2 == 1) {
+      setIsNotice(true);
+    } else {
+      setIsNotice(false);
     }
   });
+
+  const onClick_vol = () => {
+    setIsClick_vol(isClick_vol + 1);
+  };
+
+  const onClick_spon = () => {
+    setIsClick_spon(isClick_spon + 1);
+  };
+
+  const onClick_set = () => {
+    setIsClick_set(isClick_set + 1);
+  };
+
+  const onClick_notice = () => {
+    setIsClick_notice(isClick_notice + 1);
+  };
 
   return (
     <div className="MyPageMenuTest">
       <details className="menuTop" open={isVolunteer}>
-        <summary>
+        <summary onClick={onClick_vol}>
           봉사 활동{" "}
           <img
             className="mypagemenuIcon"
@@ -56,7 +91,7 @@ const MyPageMenuTest = ({ clicked }) => {
         </div>
       </details>
       <details open={isSpon}>
-        <summary>
+        <summary onClick={onClick_spon}>
           후원 기부{" "}
           <img
             className="mypagemenuIcon"
@@ -77,7 +112,7 @@ const MyPageMenuTest = ({ clicked }) => {
         <div>기부내역</div>
       </details>
       <details open={isSet}>
-        <summary>
+        <summary onClick={onClick_set}>
           설정{" "}
           <img
             className="mypagemenuIcon"
@@ -91,11 +126,15 @@ const MyPageMenuTest = ({ clicked }) => {
         <div>내 정보 수정</div>
       </details>
       <details className="menuBottom">
-        <summary>
+        <summary onClick={onClick_notice}>
           공지사항{" "}
           <img
             className="mypagemenuIcon"
-            src={process.env.PUBLIC_URL + `./assets/close.png`}
+            src={
+              isNotice
+                ? process.env.PUBLIC_URL + `./assets/open.png`
+                : process.env.PUBLIC_URL + `./assets/close.png`
+            }
           />
         </summary>
       </details>
